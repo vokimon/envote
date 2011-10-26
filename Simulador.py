0a74106a7a0668f028c9fe3eadd951b18ef55ba6
David García Garzón <david.garcia@upf.edu>
2011-10-27 01:48:27 +0200

    Using csv extension instead of cvs

--- Simulador.py
+++ Simulador.py
@@ -15,7 +15,7 @@
 		_,   votacions2000, percent2000, escanos2000,
 	in [
 		line.strip().split('\t')
-		for line in file("generals.cvs")
+		for line in file("generals.csv")
 		]
 	])
 
@@ -24,7 +24,7 @@ for year, data in [
 	(2004, resultadosGeneralesBarcelona2004),
 	(2008, resultadosGeneralesBarcelona2008),
 	] :
-	f = open("parlamentoBarcelona%s"%year, "w")
+	f = open("parlamentoBarcelona%s.csv"%year, "w")
 	for partido, votaciones, percent, escanos in data :
 		print >> f, '\t'.join([partido, votaciones, percent, escanos])
 	
