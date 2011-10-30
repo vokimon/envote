f2b0e9ca37821b1acab613729107536e3a58d0c0
David García Garzón <david.garcia@upf.edu>
2011-10-30 04:22:03 +0100

    HondtTable: threshold, resize disabled, left align

--- HondtTable.py
+++ HondtTable.py
@@ -10,6 +10,10 @@ class HondtTable(QtGui.QTableWidget) :
 		self._nSeats = 10
 		self._threshold = .03
 
+		self.verticalHeader().setResizeMode(QtGui.QHeaderView.ResizeToContents)
+		self.horizontalHeader().setResizeMode(QtGui.QHeaderView.ResizeToContents)
+
+
 	@QtCore.pyqtProperty(int)
 	def nSeats(self) :
 		return self._nSeats
@@ -30,42 +34,55 @@ class HondtTable(QtGui.QTableWidget) :
 		self.clear()
 		self.setRowCount(nParties)
 		self.setColumnCount(self.nSeats)
+
+		# Sort Parties by votes
 		self._votations = [
 			(party,votes) 
 			for votes, party in 
 				reversed(sorted(
 					(votes, party) for party, votes in results.iteritems()))
 			 ]
+
+		# Adding party names
 		self.setVerticalHeaderLabels([
 			party for party, votes in self._votations])
 
+		# Distribute seats value
 		self._seats = []
 		for i, (party, votes) in enumerate(self._votations) :
 			for seat in xrange(1, self.nSeats+1) :
 				item = QtGui.QTableWidgetItem("%i"%(votes/seat))
+				item.setTextAlignment(QtCore.Qt.AlignRight);
 				self.setItem(i, seat-1, item)
 				self._seats.append( ( (votes/seat), item) )
 		self._seats.sort()
+
+		# Mark parties under threshold
+		# TODO: Blank votes not considered yet
+		firstThresholdedParty = len(
+			[party for party, votes in self._votations
+				if votes > self._threshold])
+
+		for party in xrange(firstThresholdedParty, nParties) :
+			for seat in xrange(self.nSeats) :
+				item = self.item(party, seat)
+				item.setForeground(QtGui.QColor("#bbb"))
 
 		# Mark taken seats
 		for number, seatItem in self._seats[-self.nSeats:] :
 			seatItem.setBackground(QtGui.QColor("#ddb"))
 
-		# Mark last competition in red
+		# Mark seats in the border between in and out
 		self._seats[-self.nSeats][1].setForeground(
 			QtGui.QColor("red"))
 		self._seats[-self.nSeats-1][1].setForeground(
 			QtGui.QColor("red"))
 
-		# Mark next competition in orange
+		# Mark seats next to the border
 		self._seats[-self.nSeats+1][1].setForeground(
 			QtGui.QColor("orange"))
 		self._seats[-self.nSeats-2][1].setForeground(
 			QtGui.QColor("orange"))
-
-		self.resizeColumnsToContents()
-		self.resizeRowsToContents()
-
 
 
 if __name__ == "__main__" :
@@ -73,11 +90,17 @@ if __name__ == "__main__" :
 	window = HondtTable()
 	window.setWindowTitle("Simulador de votacions")
 	window.nSeats = 31
+	window.threshold = 2000
 	window.feedVotations(dict(
-		unos=7453,
-		esos=15575,
-		otros=23434,
-		aquellos=3434,
+		party1=7453,
+		party2=15575,
+		party3=23434,
+		party4=5434,
+		party5=3435,
+		party6=2834,
+		party7=3438,
+		party8=1433,
+		party9=431,
 		))
 
 	window.show()
