c7694a7e032571ff0f16da861086a83a698da5ad
David García Garzón <david.garcia@upf.edu>
2011-10-31 01:12:10 +0100

    GPL3 license added

--- HondtTable.py
+++ HondtTable.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Copyright 2011 David García Garzón
+
+This file is part of enVote
+
+enVote is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+enVote is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 import Simulador
 from PyQt4 import QtGui, QtCore
@@ -21,7 +40,6 @@ class HondtTable(QtGui.QTableWidget) :
 	@nSeats.setter
 	def nSeats(self, value) :
 		self._nSeats = value
-		self.redistribute()
 
 	@QtCore.pyqtProperty(float)
 	def threshold(self) :
@@ -29,7 +47,6 @@ class HondtTable(QtGui.QTableWidget) :
 	@threshold.setter
 	def threshold(self, value) :
 		self._threshold = value
-		self.redistribute()
 
 
 	def feedVotations(self, results) :
