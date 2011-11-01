a64dfd3157e80ac7327138f525f0654a8fbc02a6
David García Garzón <david.garcia@upf.edu>
2011-11-01 22:18:28 +0100

    windist: added a comment explaining dependencies

--- windist.sh
+++ windist.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# This scripts is a unix/linux scripts that builds the windows binaries
+# Requires wine
+# Requires windows versions (installed on wine) of python27, pyqt and cxfreeze
+
+
 version=0.1
 
 ~/.wine/drive_c/Python27/python.exe /home/vokimon/.wine/drive_c/Scripts/cxfreeze  envote --target-dir envote-$version
